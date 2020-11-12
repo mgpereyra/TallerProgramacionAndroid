6191ec02c648d0054c4eee17c45d11c74313674c
Maximiliano Pereyra <pereyra.m@outlook.com>
2020-11-12 19:06:30 -0300

    Se separan responsabilidades entre el Activity y ViewModel de CrearNotas

--- app/src/main/java/com/example/notesapp/ui/CrearNotasActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/CrearNotasActivity.kt
@@ -1,6 +1,5 @@
 package com.example.notesapp.ui
 
-import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,9 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.example.notesapp.R
-import com.example.notesapp.data.NotaEntity
 import com.example.notesapp.databinding.ActivityCrearNotasBinding
 import kotlinx.android.synthetic.main.activity_crear_notas.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -22,93 +19,80 @@ class CrearNotasActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCrearNotasBinding
     private val viewModelCrear: CrearNotasViewModel by viewModel()
 
-    var pathImagen = ""
-    private val REQUEST_CAMERA = 1002
-    var photo:Uri?=null
+    private val REQUEST_GALERIA = 1000
+    private val REQUEST_CAMERA = 2000
+    private var photo:Uri?=null
+    private var pathImagen = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCrearNotasBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
         binding.buttonSacarFotos.setOnClickListener { tomarFotos() }
         binding.btnGuardar.setOnClickListener { guardar() }
         binding.buttonGaleria.setOnClickListener { buscarGaleria() }
         binding.btnCancelar.setOnClickListener { onBackPressed() }
     }
 
-    fun buscarGaleria() {
+    private fun buscarGaleria() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, 100, Bundle())
+        startActivityForResult(intent, REQUEST_GALERIA, Bundle())
     }
 
-    fun tomarFotos() {
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-            if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
-                ||checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                ==PackageManager.PERMISSION_DENIED){
-                val permisosCamara = arrayOf(android.Manifest.permission.CAMERA,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                requestPermissions(permisosCamara,REQUEST_CAMERA)
-            }else{
-                val value = ContentValues()
-                photo = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,value)
-                val camaraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                camaraIntent.putExtra(MediaStore.EXTRA_OUTPUT,photo)
-                startActivityForResult(
-                    camaraIntent,
-                    REQUEST_CAMERA,
-                    Bundle()
-                )
-            }
+    private fun abrirCamara(){
+        photo = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, ContentValues())
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photo)
+        startActivityForResult(intent, REQUEST_CAMERA, Bundle())
+    }
+
+    private fun guardar() {
+        val descripcion = textDescription.text.toString()
+        if(viewModelCrear.validarGuardar(descripcion, pathImagen)){
+            mostrarToast(getString(R.string.debe_ingresar_una_descripcion_y_una_imagen))
         }else{
-            Toast.makeText(applicationContext,
-                getString(R.string.funcion_no_disponible),
-                Toast.LENGTH_SHORT)
-                .show()
+            mostrarToast(getString(R.string.nota_guardada))
+            onBackPressed()
         }
     }
 
-    fun guardar() {
-        val descripcion = textDescription.text.toString()
-        if(descripcion.trim()=="" || pathImagen==""){
-            Toast.makeText(
-                applicationContext,
-                getString(R.string.debe_ingresar_una_descripcion_y_una_imagen),
-                Toast.LENGTH_SHORT
-            ).show()
+    private fun tomarFotos() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            val permisoCamara = checkSelfPermission(android.Manifest.permission.CAMERA)
+            val permisoAlmacenamiento = checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            val denegado = PackageManager.PERMISSION_DENIED
+            if(permisoCamara == denegado || permisoAlmacenamiento == denegado){
+                val permisosCamara = arrayOf(android.Manifest.permission.CAMERA,
+                                             android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permisosCamara, REQUEST_CAMERA)
+            }else{
+                abrirCamara()
+            }
         }else{
-            viewModelCrear.insertarEnDatabase(
-                NotaEntity(
-                    description = descripcion,
-                    srcImagen = pathImagen
-                )
-            )
-            Toast.makeText(
-                applicationContext,
-                getString(R.string.nota_guardada),
-                Toast.LENGTH_SHORT
-            ).show()
-            onBackPressed()
+            abrirCamara()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 100 && resultCode == RESULT_OK) {
-            pathImagen=data!!.data.toString()
-            imageView.setImageURI(data.data)
-        }
-        if (requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK) {
-            try{
+        if(resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_GALERIA) {
+                pathImagen = data!!.data.toString()
+                imageView.setImageURI(data.data)
+            }
+            if (requestCode == REQUEST_CAMERA) {
                 imageView.setImageURI(photo)
-                pathImagen=photo.toString()
-            }catch(e: Exception){
-                Toast.makeText(this,
-                    R.string.foto_no_guardada,
-                    Toast.LENGTH_SHORT).show()
+                pathImagen = photo.toString()
             }
         }
     }
 
-
+    private fun mostrarToast(stringMostrar: String){
+        Toast.makeText(
+        applicationContext,
+        stringMostrar,
+        Toast.LENGTH_SHORT
+        ).show()
+    }
 }
--- app/src/main/java/com/example/notesapp/ui/CrearNotasViewModel.kt
+++ app/src/main/java/com/example/notesapp/ui/CrearNotasViewModel.kt
@@ -1,25 +1,29 @@
 package com.example.notesapp.ui
 
-import android.content.ContentValues
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Toast
-import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.notesapp.R
 import com.example.notesapp.data.NotaEntity
 import com.example.notesapp.model.NotaRepository
-import kotlinx.android.synthetic.main.activity_crear_notas.*
 import kotlinx.coroutines.launch
 
 class CrearNotasViewModel(private val notasRepository: NotaRepository
     ) : ViewModel() {
 
-    fun insertarEnDatabase(nota: NotaEntity) {
+    fun validarGuardar(description: String, pathImagen: String): Boolean {
+        if(description.trim()=="" || pathImagen==""){
+            return false
+        }else{
+            insertarEnDatabase(
+                NotaEntity(
+                    description = description,
+                    srcImagen = pathImagen
+                )
+            )
+            return true
+        }
+    }
+
+    private fun insertarEnDatabase(nota: NotaEntity) {
         viewModelScope.launch {
             notasRepository.insertarNota(nota)
         }
